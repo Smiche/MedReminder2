22a3adb086f695bcc2547ed0361a4646b9f711a5
Smiche <smichess@gmail.com>
2016-05-16 12:00:46 +0300

    fixing FieldVerifier

--- MedReminder/src/org/observis/medreminder/server/CommunicationServiceImpl.java
+++ MedReminder/src/org/observis/medreminder/server/CommunicationServiceImpl.java
@@ -20,7 +20,7 @@ public class CommunicationServiceImpl extends RemoteServiceServlet implements
 	 * 
 	 */
 	private static final long serialVersionUID = -8777697465243592882L;
-
+	
 	/**
 	 * 
 	 */
